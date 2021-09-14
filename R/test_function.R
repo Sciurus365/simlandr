a9c185861ded96d457c3d491d1b4a00a1089be9b
Sciurus365 <jingmeng.cui@outlook.com>
2021-09-14 17:45:30 +0200

    21.9.14.2
    New sim_fun_test2; added `...` to `batch_simulation`

--- R/test_function.R
+++ R/test_function.R
@@ -11,28 +11,32 @@
 #' @export
 #'
 sim_fun_test <- function(par1, par2, length = 1000) {
-	output <- matrix(nrow = length, ncol = 3)
-	colnames(output) <- c("out1", "out2", "out3")
-	output[1, ] <- c(par1$var1, par2$var2, rnorm(1, sd = 0.01))
-	for (i in 2:length) {
-		output[i, 1] <- 0.5 * output[i - 1, 1] + output[i - 1, 2] + par2$var3 + par1$var1 * par2$var2
-		output[i, 2] <- -0.5 * output[i - 1, 1] + output[i - 1, 2] + par2$var3
-		output[i, 3] <- output[i - 1, 3] + rnorm(1, sd = 0.01)
-	}
-	return(output)
+  output <- matrix(nrow = length, ncol = 3)
+  colnames(output) <- c("out1", "out2", "out3")
+  output[1, ] <- c(par1$var1, par2$var2, rnorm(1, sd = 0.01))
+  for (i in 2:length) {
+    output[i, 1] <- 0.5 * output[i - 1, 1] + output[i - 1, 2] + par2$var3 + par1$var1 * par2$var2
+    output[i, 2] <- -0.5 * output[i - 1, 1] + output[i - 1, 2] + par2$var3
+    output[i, 3] <- output[i - 1, 3] + rnorm(1, sd = 0.01)
+  }
+  return(output)
 }
 
 
 #' A simple yet meaningful simulation function for testing
 #'
-#' This is a toy system with bi-stability. You can use it to try out functions.
+#' This is a toy stochastic gradient system which can have bi-stability in some conditions.
 #' Model specification:
-#' \deqn{dx/dt = cy - y^3 + \sigma dW/dt}
-#' \deqn{y = x + \sigma dW}
+#' \deqn{U = x^4 + y^4 + axy + bx + cy}
+#' \deqn{dx/dt = - \partial U/ \partial x + \sigma dW/dt = - 4x^3 - ay - b + \sigma dW/dt}
+#' \deqn{dy/dt = - \partial U/ \partial y + \sigma dW/dt = - 4y^3 - ax - c + \sigma dW/dt}
 #'
-#' @param variable,parameter Two sets of parameters. \code{variable} contains \code{x} and \code{y};
-#' \code{parameter} contains \code{c}, which controls the shape of the landscape, and \code{sigmasq}, which controls the amplitude of noise.
+#' @param initial,parameter Two sets of parameters. \code{initial} contains the initial value of \code{x} and \code{y};
+#' \code{parameter} contains \code{a,b,c}, which control the shape of the potential landscape,
+#' and \code{sigmasq}, which is the square of \eqn{\sigma} and controls the amplitude of noise.
 #' @param length The length of simulation.
+#' @param stepsize The step size used in the Euler method.
+#' @param seed The initial seed that will be passed to \code{set.seed()} function.
 #'
 #' @return A matrix of simulation results.
 #'
@@ -40,20 +44,22 @@ sim_fun_test <- function(par1, par2, length = 1000) {
 #'
 #' @export
 #'
-sim_fun_test2 <- function(variable = list(x = 1, y = 1), parameter = list(sigmasq = 25, c = 1), length = 1e5) {
-	output <- matrix(nrow = length, ncol = 2)
-	colnames(output) <- c("x", "y")
-	output[1, ] <- c(variable$x, variable$y)
-	for (i in 2:length) {
-		## Simplest chemical https://bmcsystbiol.biomedcentral.com/articles/10.1186/1752-0509-3-90
-		# output[i, "x"] <- max(0, 2 * output[i - 1, "y"] - output[i - 1, "x"]^2 - output[i - 1, "x"] * output[i - 1, "y"] - output[i - 1, "x"] + rnorm(1, sd = parameter$sd))
-		# output[i, "y"] <- max(0, output[i - 1, "x"]^2 - output[i - 1, "y"] + rnorm(1, sd = parameter$sd))
-		## Lotka–Volterra
-		# output[i, "x"] <- max(0, output[i - 1, "x"] + 2/3*output[i - 1, "x"] - 4/3*output[i - 1, "x"] * output[i - 1, "y"] + rnorm(1, sd = parameter$sd))
-		# output[i, "y"] <- max(0, output[i - 1, "y"] + output[i - 1, "x"] * output[i - 1, "y"] - output[i - 1, "y"] + rnorm(1, sd = parameter$sd))
-		## Simplest single-variable
-		output[i, "x"] <- output[i - 1, "x"] + 0.01*(parameter$c * output[i - 1, "y"] - output[i - 1, "y"]^3 + rnorm(1, sd = sqrt(parameter$sigmasq)))
-		output[i, "y"] <- output[i - 1, "x"] + 0.01*(rnorm(1, sd = sqrt(parameter$sigmasq)))
-	}
-	return(output)
+sim_fun_test2 <- function(initial = list(x = 0, y = 0), parameter = list(a = -4, b = 0, c = 0, sigmasq = 1), length = 1e5, stepsize = 0.01, seed = NULL) {
+  set.seed(seed)
+  output <- matrix(nrow = length, ncol = 2)
+  colnames(output) <- c("x", "y")
+  output[1, ] <- c(initial$x, initial$y)
+  for (i in 2:length) {
+    x_temp <- output[i - 1, "x"]
+    y_temp <- output[i - 1, "y"]
+    for (j in 1:(1 / stepsize)) {
+      x_temp_new <- x_temp - stepsize * (4 * x_temp^3 + parameter$a * y_temp + parameter$b) + rnorm(1, sd = sqrt(parameter$sigmasq * stepsize))
+      y_temp_new <- y_temp - stepsize * (4 * y_temp^3 + parameter$a * x_temp + parameter$c) + rnorm(1, sd = sqrt(parameter$sigmasq * stepsize))
+      x_temp <- x_temp_new
+      y_temp <- y_temp_new
+    }
+    output[i, "x"] <- x_temp
+    output[i, "y"] <- y_temp
+  }
+  return(output)
 }
